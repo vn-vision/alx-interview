e1ca62e4b99d68a7527e775e497e3c99b06fee8c
kZilla <kzilla@vn-z.zilla>
2024-08-22 23:09:07 +0300

    check valid utf-8, added byte 1,4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """
-Check whether a number is a valid UTF8
+Check whether a number is a valid UTF8i
+valid utf-encoding follows the pattern for each byte 1 - 4
+    4 - 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+    3 - 1110xxxx 10xxxxxx 10xxxxxx
+    2 - 110xxxxx 10xxxxxx
+    1 - 0x00 to 0x7F
 """
 
 
@@ -20,10 +25,35 @@ def validUTF8(data):
     # if the length is past '08b': invalid
 
     for enc_num in encoded:
-        if len(enc_num) > 8:
-            _bool.append(False)
-        else:
+        if len(enc_num) == 32:
+            # 4 byte character
+            if enc_num[0:5] == '11110' and \
+                    enc_num[8:10] == enc_num[16:18] == enc_num[24:26] == '10':
+                _bool.append(True)
+            else:
+                _bool.append(False)
+
+        elif len(enc_num) == 24:
+            # 3 byte character
+            if enc_num[0:5] == '1110' and \
+                    enc_num[8:10] == enc_num[16:18] == '10':
+                _bool.append(True)
+            else:
+                _bool.append(False)
+
+        elif len(enc_num) == 16:
+            # 2 byte character
+            if enc_num[0:5] == '110' and enc_num[8:10] == '10':
+                _bool.append(True)
+            else:
+                _bool.append(False)
+
+        elif len(enc_num) <= 8:
+            # 1 byte character
             _bool.append(True)
+        else:
+            # other formats
+            _bool.append(False)
 
     # check which data has all valid utf-8
     if False in _bool:
